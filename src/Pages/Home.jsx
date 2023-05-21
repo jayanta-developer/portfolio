9991181db5f991e97d5a2138f05da732abdcf9cd
jayanta dey <116143724+jayanta-musquare@users.noreply.github.com>
2023-05-21 13:59:49 +0530

    working on home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import { Container, Box, Typography, Button } from '@mui/material'
+import React from 'react';
+import AvatarImage from "../Assets/Images/20221004_214441-modified-removebg-preview.png"
+
+import "./Home.css"
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <Container className="homeContainer" sx={{ marginTop: "100px", maxWidth: "90%" }}>
+      <Box className="homeBox">
+        <Box className="textArya">
+          <Box className="headerText ButtonMargin">
+            <Typography variant='h1'>Hay' im  Jayanta</Typography>
+            <Typography variant='h3'>I am a web Developer</Typography>
+          </Box>
+          <Box className="bioText ButtonMargin">
+            <Typography variant="caption">
+              I’m a Web developer & I’m very passionate and dedicated to my work. I have <br />acquired the skills and knowledge necessary to make your project a success.
+            </Typography>
+          </Box>
+          <Box className="buttonBox ">
+
+            <Box className="AboutBtn">
+              <Button variant="contained">About me</Button>
+            </Box>
+            <Box className="WorkBtn">
+              <Button>My work</Button>
+            </Box>
+          </Box>
+        </Box>
+
+        <Box className="AvatarPhoto">
+          <img src={AvatarImage} alt="" />
+        </Box>
+      </Box>
+
+    </Container>
   )
 }
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -4,21 +4,22 @@ import { Box, Typography } from '@mui/material';
 import "./navBar.css"
 
 
-const NavBar = () => {
-  <Box className="navContainer">
-    <Box className="navBar">
-      <Box className="logo">
-        <Typography>
-          Jayanta.Developoer
-        </Typography>
+export default function NavBar() {
+  return (
+    <Box className="navContainer">
+      <Box className="navBar">
+        <Box className="logo">
+          <Typography>
+            Jayanta.Developoer
+          </Typography>
+        </Box>
+        <Box className="menuItems">
+          <Box className="menuItem">menu1</Box>
+          <Box className="menuItem">menu2</Box>
+          <Box className="menuItem">menu3</Box>
+        </Box>
+        <Box className="button">button</Box>
       </Box>
-      <Box className="menuItems">
-        <Box className="menuItem">menu1</Box>
-        <Box className="menuItem">menu2</Box>
-        <Box className="menuItem">menu3</Box>
-      </Box>
-      <Box className="button">button</Box>
     </Box>
-  </Box>
+  )
 }
-export default NavBar
