864f573993a45a4e7c11179ebdb40ed9b06f71ec
jayanta dey <116143724+jayanta-musquare@users.noreply.github.com>
2023-06-14 13:48:20 +0530

    update npm files

---
+++ src/Pages/Skills/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Skill() {
+  return (
+    <Container
+      className="workContainer"
+      sx={{ marginTop: "80px", maxWidth: "90%" }}
+    >
+      skill section
+    </Container>
+  );
+}
